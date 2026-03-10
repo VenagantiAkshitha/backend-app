572e3507e5af00a7462b36dedc35dfe03388e0ce
VenagantiAkshitha <akshithavenaganti@gmail.com>
2026-03-10 14:08:55 +0530

    curd operations

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,36 @@
-// import { render } from "ejs";
-// import productModel from "../models/productModel";
-// const showProducts = (req,res) => {
-//     res.render("home", {productModel})
-// }
-// export {showProducts}
+import productModel from "../models/productModel.js";
+
+const getProducts = async (req,res) => {
+    const products = await productModel.find();
+    res.render("products/index", {products});
+};
+
+const addProduct = async(req,res) => {
+    const product = req.body
+    await productModel.create(product)
+    res.redirect("/products");
+};
+
+const addProductForm = async(req,res) => {
+    res.render("products/add");
+}
+
+const deleteProducts = async(req,res) => {
+    const id = req.params.id;
+    await productModel.findByIdAndDelete(id)
+    res.redirect("/products")
+};
+
+const editProductForm = async (req,res) => {
+    const id = req.params.id;
+    const product = await productModel.findOne({_id:id});
+    res.render("products/edit",{product});
+};
+
+const saveProduct = async(req,res) => {
+    const id = req.params.id;
+    await productModel.findByIdAndUpdate(id,req.body);
+    res.redirect("/products");
+};
+
+export {getProducts,addProduct,deleteProducts,addProductForm,editProductForm,saveProduct};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,11 @@
-// import { showProducts } from "../controllers/productController";
-// import express from "express"
-// const productRouter = express.Router()
-// productRouter.get("/",showProducts)
+import { getProducts,addProduct,addProductForm,editProductForm,saveProduct,deleteProducts} from "../controllers/productController.js";
+import express from "express"
+const productRouter = express.Router();
+productRouter.get("/",getProducts);
+productRouter.post("/add",addProduct);
+productRouter.get("/add",addProductForm);
+productRouter.get("/:id/delete",deleteProducts);
+productRouter.get("/:id/edit",editProductForm);
+productRouter.post("/:id/save",saveProduct);
 
-// export {productRouter}
+export default productRouter;
