3c48f8d182dbdcf07a12e97a12e2cceeee738a77
VenagantiAkshitha <akshithavenaganti@gmail.com>
2026-03-12 15:14:36 +0530

    users

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,36 +1,43 @@
 import productModel from "../models/productModel.js";
 
-const getProducts = async (req,res) => {
-    const products = await productModel.find();
-    res.render("products/index", {products});
+const getProducts = async (req, res) => {
+  const products = await productModel.find();
+  res.render("products/index", { products });
 };
 
-const addProduct = async(req,res) => {
-    const product = req.body
-    await productModel.create(product)
-    res.redirect("/products");
+const addProduct = async (req, res) => {
+  const product = req.body;
+  await productModel.create(product);
+  res.redirect("/products");
 };
 
-const addProductForm = async(req,res) => {
-    res.render("products/add");
-}
-
-const deleteProducts = async(req,res) => {
-    const id = req.params.id;
-    await productModel.findByIdAndDelete(id)
-    res.redirect("/products")
+const addProductForm = async (req, res) => {
+  res.render("products/add");
 };
 
-const editProductForm = async (req,res) => {
-    const id = req.params.id;
-    const product = await productModel.findOne({_id:id});
-    res.render("products/edit",{product});
+const deleteProduct = async (req, res) => {
+  const id = req.params.id;
+  await productModel.findByIdAndDelete(id);
+  res.redirect("/products");
 };
 
-const saveProduct = async(req,res) => {
-    const id = req.params.id;
-    await productModel.findByIdAndUpdate(id,req.body);
-    res.redirect("/products");
+const editProductForm = async (req, res) => {
+  const id = req.params.id;
+  const product = await productModel.findOne({ _id: id });
+  res.render("products/edit", { product });
 };
 
-export {getProducts,addProduct,deleteProducts,addProductForm,editProductForm,saveProduct};
+const saveProduct = async (req, res) => {
+  const id = req.params.id;
+  await productModel.findByIdAndUpdate(id, req.body);
+  res.redirect("/products")
+};
+
+export {
+  getProducts,
+  addProduct,
+  addProductForm,
+  deleteProduct,
+  editProductForm,
+  saveProduct
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,19 @@
-import { getProducts,addProduct,addProductForm,editProductForm,saveProduct,deleteProducts} from "../controllers/productController.js";
-import express from "express"
+import {
+  getProducts,
+  addProductForm,
+  addProduct,
+  deleteProduct,
+  editProductForm,
+  saveProduct,
+} from "../controllers/productController.js";
+import express from "express";
 const productRouter = express.Router();
-productRouter.get("/",getProducts);
-productRouter.post("/add",addProduct);
-productRouter.get("/add",addProductForm);
-productRouter.get("/:id/delete",deleteProducts);
-productRouter.get("/:id/edit",editProductForm);
-productRouter.post("/:id/save",saveProduct);
+
+productRouter.get("/", getProducts);
+productRouter.get("/add", addProductForm);
+productRouter.post("/add", addProduct);
+productRouter.get("/:id/delete", deleteProduct);
+productRouter.get("/:id/edit", editProductForm);
+productRouter.post("/:id/save", saveProduct);
 
 export default productRouter;
